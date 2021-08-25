d3e2c9cc53f399fe016b8fafe00129fcebfe9f9e
Mansur <funisinfinity26@gmail.com>
2021-08-25 16:24:04 +0600

    remove cors

--- index.js
+++ index.js
@@ -35,31 +35,9 @@ client.on('message', message => {
 
 
 const express = require('express')
-const cors = require('cors');
-
 const app = express()
 
 const port = process.env.PORT || 3000
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-const allowedOrigins = ['http://192.168.1.189/',
-                      'https://potato-cashback.herokuapp.com/',
-                      'https://test-potato-cashback.herokuapp.com/'];
-app.use(cors({
-    origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-    }
-}));
 
 
 const fetch = require('node-fetch');
